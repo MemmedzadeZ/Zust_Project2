65ebcfbfa65dcf481dc3dff73653bed41944ba0b
Zehra.M <139117868+MemmedzadeZ@users.noreply.github.com>
2024-09-19 23:05:56 +0400

    Добавьте файлы проекта.

---
+++ Zust_Project_MVC/Controllers/HomeController.cs
@@ -0,0 +1,128 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Zust_Project_MVC.Models;
+
+namespace Zust_Project_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Birthday()
+        {
+            return View();
+        }
+
+
+        public IActionResult Events()
+        {
+            return View();
+        }
+
+        public IActionResult Favorite()
+        {
+            return View();
+        }
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        public IActionResult Friends()
+        {
+            return View();
+        }
+
+        public IActionResult Groups()
+        {
+            return View();
+        }
+
+        public IActionResult HelpandSupport()
+        {
+            return View();
+        }
+
+        public IActionResult LiveChat()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult MarketPlace()
+        {
+            return View();
+        }
+
+        public IActionResult Message()
+        {
+            return View();
+        }
+
+        public IActionResult MyProfile()
+        {
+
+            return View();
+        }
+
+        public IActionResult Notification()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+
+
+            return View();
+        }
+
+        public IActionResult Setting()
+        {
+            return View();
+        }
+
+        public IActionResult Video()
+        {
+            return View();
+        }
+
+        public IActionResult Weather()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

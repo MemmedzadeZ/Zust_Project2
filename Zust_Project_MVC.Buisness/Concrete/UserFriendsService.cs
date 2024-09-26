0e70226b1c40885f3642df65f722bdd95b92cc51
Memme_yd89 <Memme_yd89@STHQ0128-05>
2024-09-26 14:38:07 +0400

    done

---
+++ Zust_Project_MVC.Buisness/Concrete/UserFriendsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zust_Project_MVC.Buisness.Abstract;
+
+namespace Zust_Project_MVC.Buisness.Concrete
+{
+    public class UserFriendsService:IUserFriendsService
+    {
+    }
+}
---
+++ Zust_Project_MVC/Models/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zust_Project_MVC.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+
+        public string? Name { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+        [DataType(DataType.Password)]
+        public string? ConfirmPassword { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+
+
+    }
+}
